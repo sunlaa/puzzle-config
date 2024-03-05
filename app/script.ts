4aab5ee43c6225d6e1db6aa08722647211ff7abd
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-03-05 15:29:47 +0100

    finish config

---
+++ app/script.ts
@@ -0,0 +1,7 @@
+import './style.css';
+
+const hole: HTMLDivElement = document.createElement('div');
+
+hole.className = 'hole';
+
+export default hole;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import hole from '../app/script';
+
+const { body } = document;
+
+body.append(hole);
